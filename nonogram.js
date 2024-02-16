f6b58e8e66f4e31f8860cbd463acbaab3cb9597a
puokl <phuoc@sapo.pt>
2024-02-16 17:18:35 +0100

    first commit

--- nonogram.js
+++ nonogram.js
@@ -8,7 +8,6 @@ class Nonogram {
 
   async loadMatrix(url) {
     try {
-      console.log("Loading matrix...");
       const response = await fetch(url);
       const solutionMatrix = await response.json();
       this.rows = solutionMatrix.length;
@@ -25,7 +24,6 @@ class Nonogram {
 
       this.updateUI();
       this.updateHints();
-      console.log("Matrix loaded");
     } catch (error) {
       console.error("Error loading matrix:", error);
     }
@@ -59,6 +57,55 @@ class Nonogram {
   updateHints() {
     this.updateVerticalHints();
     this.updateHorizontalHints();
+    this.checkAndHighlightCompletedRows();
+    this.checkAndHighlightCompletedColumns();
+  }
+  checkAndHighlightCompletedRows() {
+    for (let row = 0; row < this.rows; row++) {
+      const rowArray = this.grid[row];
+      if (this.isRowCompleted(rowArray)) {
+        this.highlightRow(row);
+      }
+    }
+  }
+
+  checkAndHighlightCompletedColumns() {
+    for (let col = 0; col < this.columns; col++) {
+      const column = this.grid.map((row) => row[col]);
+      if (this.isColumnCompleted(column)) {
+        this.highlightColumn(col);
+      }
+    }
+  }
+
+  isRowCompleted(rowArray) {
+    return rowArray.every(
+      (cell) =>
+        cell.isFilled &&
+        (cell.colorClass === "black" || cell.colorClass === "green")
+    );
+  }
+
+  isColumnCompleted(column) {
+    return column.every(
+      (cell) =>
+        cell.isFilled &&
+        (cell.colorClass === "black" || cell.colorClass === "green")
+    );
+  }
+
+  highlightRow(row) {
+    const rowHints = document.querySelectorAll(`.horizontal-hint.row-${row}`);
+    if (rowHints) {
+      rowHints.forEach((hint) => (hint.style.color = "red"));
+    }
+  }
+
+  highlightColumn(col) {
+    const colHints = document.querySelectorAll(`.vertical-hint.col-${col}`);
+    if (colHints) {
+      colHints.forEach((hint) => (hint.style.color = "red"));
+    }
   }
 
   toggleCell(row, column) {
@@ -135,19 +182,105 @@ class Nonogram {
   }
 
   // Method to update vertical hints
+  // updateVerticalHints() {
+  //   const verticalHintsContainer = document.getElementById("vertical-hints");
+  //   verticalHintsContainer.innerHTML = "";
+
+  //   for (let col = 0; col < this.columns; col++) {
+  //     const solutionColumn = this.getColumnHints(col, true);
+  //     const hintElement = document.createElement("div");
+  //     hintElement.classList.add(`vertical-hint`, `col-${col}`);
+  //     // hintElement.className = `horizontal-hint row-${row}`;
+  //     hintElement.innerText = solutionColumn.join(" ");
+  //     verticalHintsContainer.appendChild(hintElement);
+  //   }
+  // }
+
+  //!SECTION
+  // updateVerticalHints() {
+  //   const verticalHintsContainer = document.getElementById("vertical-hints");
+  //   verticalHintsContainer.innerHTML = "";
+
+  //   for (let col = 0; col < this.columns; col++) {
+  //     const solutionColumn = this.getColumnHints(col, true);
+  //     const colArray = this.grid[col];
+  //     console.log("colArray", colArray);
+  //     const hintElement = document.createElement("div");
+  //     hintElement.className = `vertical-hint col-${col}`;
+  //     hintElement.innerText = solutionColumn.join(" ");
+  //     // Check if all cells in the row are filled correctly
+  //     const isColCompleted = colArray.every(
+  //       (cell) =>
+  //         !cell.solution ||
+  //         (cell.isFilled &&
+  //           (cell.colorClass === "black" || cell.colorClass === "green"))
+  //     );
+  //     console.log("isColCompleted", isColCompleted);
+  //     // Update class based on the completion status
+  //     if (isColCompleted) {
+  //       hintElement.classList.add("completed-col");
+  //     }
+
+  //     verticalHintsContainer.appendChild(hintElement);
+  //   }
+  // }
+
+  //TODO -
   updateVerticalHints() {
     const verticalHintsContainer = document.getElementById("vertical-hints");
     verticalHintsContainer.innerHTML = "";
 
     for (let col = 0; col < this.columns; col++) {
       const solutionColumn = this.getColumnHints(col, true);
+      const columnArray = this.grid.map((row) => row[col]);
+
       const hintElement = document.createElement("div");
-      hintElement.innerText = solutionColumn.join(" ");
+      hintElement.className = `vertical-hint col-${col}`;
+
+      const isColumnCompleted = columnArray.every(
+        (cell) =>
+          !cell.solution ||
+          (cell.isFilled &&
+            (cell.colorClass === "black" || cell.colorClass === "green"))
+      );
+
+      if (isColumnCompleted) {
+        hintElement.classList.add("completed-column");
+      }
+
+      solutionColumn.forEach((hintNumber, index) => {
+        const span = document.createElement("span");
+        span.innerText = hintNumber;
+        if (isColumnCompleted) {
+          span.classList.add("completed-hint-number");
+        }
+        hintElement.appendChild(span);
+
+        if (index < solutionColumn.length - 1) {
+          const space = document.createElement("span");
+          space.innerText = " ";
+          hintElement.appendChild(space);
+        }
+      });
+
       verticalHintsContainer.appendChild(hintElement);
     }
   }
 
   // Method to update horizontal hints
+  // updateHorizontalHints() {
+  //   const horizontalHintsContainer =
+  //     document.getElementById("horizontal-hints");
+  //   horizontalHintsContainer.innerHTML = "";
+
+  //   for (let row = 0; row < this.rows; row++) {
+  //     const solutionRow = this.getRowHints(row, true);
+  //     const hintElement = document.createElement("div");
+  //     hintElement.classList.add(`horizontal-hint`, `row-${row}`);
+  //     hintElement.innerText = solutionRow.join(" ");
+  //     horizontalHintsContainer.appendChild(hintElement);
+  //   }
+  // }
   updateHorizontalHints() {
     const horizontalHintsContainer =
       document.getElementById("horizontal-hints");
@@ -155,8 +288,25 @@ class Nonogram {
 
     for (let row = 0; row < this.rows; row++) {
       const solutionRow = this.getRowHints(row, true);
+      const rowArray = this.grid[row];
+      // console.log("rowArray", rowArray);
+
       const hintElement = document.createElement("div");
+      hintElement.className = `horizontal-hint row-${row}`;
       hintElement.innerText = solutionRow.join(" ");
+
+      const isRowCompleted = rowArray.every(
+        (cell) =>
+          !cell.solution ||
+          (cell.isFilled &&
+            (cell.colorClass === "black" || cell.colorClass === "green"))
+      );
+      // console.log("isRowCompleted", isRowCompleted);
+
+      if (isRowCompleted) {
+        hintElement.classList.add("completed-row");
+      }
+
       horizontalHintsContainer.appendChild(hintElement);
     }
   }
